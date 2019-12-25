1667761b342fbe4884d5d4e01f7d5a2821bb7847
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-25 18:30:57 -0500

    reverting to just the basics for readability

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/reservoird/stdout
-
-go 1.13
-
-require github.com/reservoird/ibool v1.0.0
--- stdout.go
+++ stdout.go
@@ -1,71 +1,23 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"time"
-
-	"github.com/reservoird/ibool"
 )
 
-// needed to aid in unit testing
-type iwriter interface {
-	WriteString(string) (int, error)
-}
-
-type boolbridge struct {
-}
-
-func (o *boolbridge) Val() bool {
-	return true
-}
-
 type stdout struct {
-	Timestamp   bool
-	writer      iwriter
-	keepRunning ibool.IBool
 }
 
 // Config configures consumer
 func (o *stdout) Config(cfg string) error {
-	// default
-	o.Timestamp = false
-	o.writer = bufio.NewWriter(os.Stdout)
-	o.keepRunning = &boolbridge{}
-
-	if cfg != "" {
-		b, err := ioutil.ReadFile(cfg)
-		if err != nil {
-			return err
-		}
-		s := stdout{}
-		err = json.Unmarshal(b, &s)
-		if err != nil {
-			return err
-		}
-		o.Timestamp = s.Timestamp
-	}
 	return nil
 }
 
 // Expel reads messages from a channel and writes them to stdout
 func (o *stdout) Expel(channel <-chan []byte) error {
-	for o.keepRunning.Val() == true {
+	for {
 		line := <-channel
-
-		output := string(line)
-		if o.Timestamp == true {
-			output = fmt.Sprintf("rdetime %s: %s", time.Now().Format(time.RFC3339), line)
-		}
-		_, err := o.writer.WriteString(output)
-		if err != nil {
-			return err
-		}
+		fmt.Sprintf("%s", line)
 	}
-	return nil
 }
 
 // Expeller for stdout
