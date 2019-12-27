e7f84a924aa8ac138045ce2473f8defa1ee56441
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-26 23:53:24 -0500

    updating plugin to conform to icd

--- stdout.go
+++ stdout.go
@@ -17,25 +17,26 @@ func (o *stdout) Config(cfg string) error {
 }
 
 // Expel reads messages from a channel and writes them to stdout
-func (o *stdout) Expel(queue icd.Queue, done <-chan struct{}, wg *sync.WaitGroup) error {
+func (o *stdout) Expel(queues []icd.Queue, done <-chan struct{}, wg *sync.WaitGroup) error {
 	defer wg.Done()
 	o.run = true
 	for o.run == true {
-		d, err := queue.Pop()
-		if err != nil {
-			return err
+		for q := range queues {
+			d, err := queues[q].Pop()
+			if err != nil {
+				return err
+			}
+			line, ok := d.([]byte)
+			if ok == false {
+				return fmt.Errorf("error invalid type")
+			}
+			fmt.Printf("%s", line)
 		}
-		line, ok := d.([]byte)
-		if ok == false {
-			return fmt.Errorf("error invalid type")
-		}
-		fmt.Printf("%s", line)
 		select {
 		case <-done:
 			o.run = false
 		default:
 		}
-
 	}
 	return nil
 }
